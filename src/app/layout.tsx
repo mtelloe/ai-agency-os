5915e1d2dcd95da37c32c5a52d9ba53be37b45e9
Maria Tello <mtelloe@gmail.com>
2026-04-02 16:50:38 +0200

    feat: add Umami analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es" className={`${inter.className} dark`}>
+      <head>
+        <script defer src="https://analytics-umami.hjbrvj.easypanel.host/script.js" data-website-id="36878cfd-86e1-4c4b-9e19-b18a739d9b00"></script>
+      </head>
       <body>
         <QueryProvider>
           {children}
