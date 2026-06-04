5927d79da55cad84566b88281510faa4cb09192b
Maria Tello <mtelloe@gmail.com>
2026-06-04 17:34:47 +0200

    Add GTM-MPHLJKNL to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,10 +20,29 @@ export default function RootLayout({
   return (
     <html lang="es" className={`${inter.className} dark`}>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MPHLJKNL"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <QueryProvider>
           {children}
           <Toaster />
         </QueryProvider>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MPHLJKNL');`,
+          }}
+        />
         <Script defer src="https://analytics-umami.hjbrvj.easypanel.host/script.js" data-website-id="36878cfd-86e1-4c4b-9e19-b18a739d9b00" strategy="afterInteractive" />
       </body>
     </html>
